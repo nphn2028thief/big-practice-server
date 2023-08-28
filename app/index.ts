29c4b29063a27df0512022186fe4f8d90d28531a
Nhan Nguyen <nhan.nguyen@hikoni.com>
2023-08-28 09:09:43 +0700

    Fix status when route not exist

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,11 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express, { NextFunction, Request, Response } from 'express';
-import createHttpError from 'http-errors';
-import morgan from 'morgan';
-import envConfig from './configs/envConfig';
-import connectToDB from './database';
-import routes from './routes';
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import morgan from "morgan";
+import envConfig from "./configs/envConfig";
+import connectToDB from "./database";
+import routes from "./routes";
 
 declare global {
   namespace Express {
@@ -19,24 +19,32 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: "100mb" }));
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/api/', routes());
+app.use("/api/", routes());
 
 connectToDB();
 
 app.use((req, res, next) => {
-  next(createHttpError.NotFound('This route does not exist!'));
+  res.status(400)
+  next(createHttpError[400]("This route does not exist!"));
 });
 
-app.use((err: createHttpError.HttpError, req: Request, res: Response, next: NextFunction) => {
-  return res.json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(
+  (
+    err: createHttpError.HttpError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    return res.json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+);
 
 app.listen(envConfig.port, () => {
   console.log(`Server app listening on port ${envConfig.port}`);
